55d68af1e9e5a287176ef9003a543fd82f7bef95
徐佳俊 <Morrison@MorrisonXudeMacBook-Pro.local>
2016-04-05 12:26:08 +0800

    Initial Commit

---
+++ hihoCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hihoCode
+//
+//  Created by 徐佳俊 on 16/4/5.
+//  Copyright (c) 2016年 morrison. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
